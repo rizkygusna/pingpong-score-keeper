e772f5a75a64a6e6347ae2162db1d5f4d8beb729
rizkygusna <rizki.farid1998@gmail.com>
2021-06-18 00:06:41 +0700

    adding add score function

---
+++ app.js
@@ -0,0 +1,22 @@
+const p1Score = document.querySelector('p1Display');
+const p2Score = document.querySelector('p2Display');
+
+const scoreLimitSelect = document.querySelector('playto');
+const p1Button = document.querySelector('p1Button');
+const p2Button = document.querySelector('p2Button');
+const resetButton = document.querySelector('reset');
+
+const player1 = {
+  score: document.querySelector('p1Display'),
+  isWin: false,
+};
+
+const player2 = {
+  score: document.querySelector('p2Display'),
+  isWin: false,
+};
+
+const addScore = (player) => {
+  const addedScore = player.score.innerText.parseInt() + 1;
+  player.score.innerText = addedScore.toString();
+};
