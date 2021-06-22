2e1277e084c83c10c43aa1ed684f3591170ca56d
rizkygusna <rizki.farid1998@gmail.com>
2021-06-22 13:07:45 +0700

    add game over state

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ const p2Display = document.querySelector('#p2Display');
 let p1Score = 0;
 let p2Score = 0;
 let scoreLimit = document.querySelector('#playto').value;
+let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
-  if (p1Score < scoreLimit) {
+  if (!isGameOver) {
     p1Score += 1;
+    if (p1Score == scoreLimit) {
+      isGameOver = true;
+    }
     p1Display.innerText = p1Score;
   }
 });
 
 p2Button.addEventListener('click', function () {
-  if (p2Score < scoreLimit) {
+  if (!isGameOver) {
     p2Score += 1;
+    if (p2Score == scoreLimit) {
+      isGameOver = true;
+    }
     p2Display.innerText = p2Score;
   }
 });
