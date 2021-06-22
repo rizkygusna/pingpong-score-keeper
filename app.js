08a37707e2bfa4a2bfb8627665e7ca20aa6ed843
rizkygusna <rizki.farid1998@gmail.com>
2021-06-22 12:26:56 +0700

    adding player button event

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const scoreLimitSelect = document.querySelector('#playto');
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
-const resetButton = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
 
-const player1 = {
-  score: document.querySelector('p1Display'),
-  isWin: false,
-};
+let p1Score = 0;
+let p2Score = 0;
 
-const player2 = {
-  score: document.querySelector('p2Display'),
-  isWin: false,
-};
+p1Button.addEventListener('click', function () {
+  p1Score += 1;
+  p1Display.innerText = p1Score;
+});
 
-const addScore = (player) => {
-  const addedScore = player.score.innerText.parseInt() + 1;
-  player.score.innerText = addedScore.toString();
-};
+p2Button.addEventListener('click', function () {
+  p2Score += 1;
+  p2Display.innerText = p2Score;
+});
